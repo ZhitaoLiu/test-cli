42dab51a90a3bd1994e19caf1be182832a8c4f32
刘志涛 <damon.liu@estay.com>
2021-09-21 21:37:07 +0800

    test: 测试

--- packages/cli/bin/index.js
+++ packages/cli/bin/index.js
@@ -4,9 +4,9 @@ const importLocal = require('import-local');
 const npmlog = require('npmlog');
 
 if (importLocal(__filename)) {
-    npmlog.info('cli', 'Using local version of this package')
+    npmlog.info('cli', 'damon: Using local version of this package')
 } else {
-    npmlog.info('cli', 'Using global version of this package')
+    npmlog.info('cli', 'damon: Using global version of this package')
     console.log(process.argv)
     require('../lib')(process.argv.slice(2))
 }
